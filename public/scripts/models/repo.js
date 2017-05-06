a84f4a25e12cf08f2ff22121c5a59c9ecf05268a
Nurbek Ismailov <nurbek82@gmail.com>
2017-05-06 16:17:27 -0700

    update

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -15,11 +15,11 @@
   //     type: 'GET',
   //     headers: {'Authorization': `token ${githubToken}`}
   //   })
-  //   .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
-  //   .then(callback);
-  // };
-  $get('/github/user/repos');
-  
+  $get('/github/user/repos')
+    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    .then(callback);
+  };
+
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
