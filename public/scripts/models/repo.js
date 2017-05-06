31a963fdfd8f0d564eb35abc1ffa1c5d8dd4835b
mattreyes7 <mattreyes615@gmail.com>
2017-05-06 15:39:09 -0700

    last todo

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -10,11 +10,7 @@
     //       Don't forget to remove the headers from our request - we're no longer using a token on the
     //       client side of our app, our new proxyGitHub function will be handling the token using our
     //       new environment variable!
-    $.ajax({
-      url: `https://api.github.com/user/repos`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
+    $.get(`/github/user/repos`)
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
